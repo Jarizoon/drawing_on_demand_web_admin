c4a4932c7e214b9244d3cb46fbfaa6adf7b4923e
ThienNho <ThienNho@DESKTOP-BEJ8LO6>
2024-01-19 21:43:57 +0700

    fix route

--- lib/app_routes/go_routes.dart
+++ lib/app_routes/go_routes.dart
@@ -28,148 +28,246 @@ get shellNavigatorState => _shellNavigatorKey.currentState;
 
 class AppRoutes {
   static GoRouter routes() {
-    return GoRouter(
-      navigatorKey: _rootNavigatorKey,
-      initialLocation: LoginRoute.tag,
-      routes: [
-        GoRoute(
-            path: LoginRoute.tag,
-            name: LoginRoute.name,
-            builder: (context, state) {
-              return const LoginPage();
-            }),
-        GoRoute(
-          path: DashboardRoute.tag,
-          name: DashboardRoute.name,
-          builder: (context, state) {
-            return const DashboardPage();
-          },
-          redirect: (context, state) => _unAuthened(),
-        ),
-        if (PrefUtils().getRole() == 'Admin')
-        GoRoute(
-            path: AccountRoute.tag,
-            name: AccountRoute.name,
-            builder: (context, state) {
-              return const AccountPage();
-            },
-            redirect: (context, state) => _unAuthened(),
+    return PrefUtils().getRole() == 'Admin'
+        ? GoRouter(
+            navigatorKey: _rootNavigatorKey,
+            initialLocation: LoginRoute.tag,
             routes: [
               GoRoute(
-                path: CreateStaffRoute.tag,
-                name: CreateStaffRoute.name,
+                  path: LoginRoute.tag,
+                  name: LoginRoute.name,
+                  builder: (context, state) {
+                    return const LoginPage();
+                  }),
+              GoRoute(
+                path: DashboardRoute.tag,
+                name: DashboardRoute.name,
                 builder: (context, state) {
-                  return const CreateStaffPage();
+                  return const DashboardPage();
                 },
+                redirect: (context, state) => _unAuthened(),
               ),
-            ]),
-        GoRoute(
-          path: AccountDetailRoute.tag,
-          name: AccountDetailRoute.name,
-          builder: (context, state) {
-            return AccountDetailPage(id: state.pathParameters['account_id']);
-          },
-          redirect: (context, state) => _unAuthened(),
-        ),
-        GoRoute(
-          path: ProfileUserRouter.tag,
-          name: ProfileUserRouter.name,
-          builder: (context, state) {
-            return ProfileUserPage(id: state.pathParameters['user_id']);
-          },
-          redirect: (context, state) => _unAuthened(),
-        ),
-        GoRoute(
-          path: ArtistRoute.tag,
-          name: ArtistRoute.name,
-          builder: (context, state) {
-            return const ArtistPage();
-          },
-          redirect: (context, state) => _unAuthened(),
-        ),
-        GoRoute(
-          path: CustomerRoute.tag,
-          name: CustomerRoute.name,
-          builder: (context, state) {
-            return const CustomerPage();
-          },
-          redirect: (context, state) => _unAuthened(),
-        ),
-        if (PrefUtils().getRole() == 'Admin')
-        GoRoute(
-            path: ArtistRegisterRoute.tag,
-            name: ArtistRegisterRoute.name,
-            builder: (context, state) {
-              return const ArtistRegisterPage();
+              GoRoute(
+                  path: AccountRoute.tag,
+                  name: AccountRoute.name,
+                  builder: (context, state) {
+                    return const AccountPage();
+                  },
+                  redirect: (context, state) => _unAuthened(),
+                  routes: [
+                    GoRoute(
+                      path: CreateStaffRoute.tag,
+                      name: CreateStaffRoute.name,
+                      builder: (context, state) {
+                        return const CreateStaffPage();
+                      },
+                    ),
+                  ]),
+              GoRoute(
+                path: AccountDetailRoute.tag,
+                name: AccountDetailRoute.name,
+                builder: (context, state) {
+                  return AccountDetailPage(id: state.pathParameters['account_id']);
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
+              GoRoute(
+                path: ProfileUserRouter.tag,
+                name: ProfileUserRouter.name,
+                builder: (context, state) {
+                  return ProfileUserPage(id: state.pathParameters['user_id']);
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
+              GoRoute(
+                path: ArtistRoute.tag,
+                name: ArtistRoute.name,
+                builder: (context, state) {
+                  return const ArtistPage();
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
+              GoRoute(
+                path: CustomerRoute.tag,
+                name: CustomerRoute.name,
+                builder: (context, state) {
+                  return const CustomerPage();
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
+              GoRoute(
+                  path: ArtistRegisterRoute.tag,
+                  name: ArtistRegisterRoute.name,
+                  builder: (context, state) {
+                    return const ArtistRegisterPage();
+                  },
+                  redirect: (context, state) => _unAuthened(),
+                  routes: [
+                    GoRoute(
+                        path: ArtistRegisterDetailRoute.tag,
+                        name: ArtistRegisterDetailRoute.name,
+                        builder: (context, state) {
+                          return ArtistRegisterDetailPage(id: state.pathParameters['artistRegister_id']);
+                        }),
+                  ]),
+              GoRoute(
+                  path: ArtworkRoute.tag,
+                  name: ArtworkRoute.name,
+                  builder: (context, state) {
+                    return const ArtworkPage();
+                  },
+                  redirect: (context, state) => _unAuthened(),
+                  routes: [
+                    GoRoute(
+                        path: ArtworkDetailRoute.tag,
+                        name: ArtworkDetailRoute.name,
+                        builder: (context, state) {
+                          return ArtworkDetailPage(id: state.pathParameters['artwork_id']);
+                        }),
+                  ]),
+              GoRoute(
+                  path: OrderRoute.tag,
+                  name: OrderRoute.name,
+                  builder: (context, state) {
+                    return const OrderPage();
+                  },
+                  redirect: (context, state) => _unAuthened(),
+                  routes: [
+                    GoRoute(
+                        path: OrderDetailRoute.tag,
+                        name: OrderDetailRoute.name,
+                        builder: (context, state) {
+                          return OrderDetailPage(id: state.pathParameters['order_id']);
+                        })
+                  ]),
+              GoRoute(
+                  path: RequirementRoute.tag,
+                  name: RequirementRoute.name,
+                  builder: (context, state) {
+                    return const RequirementPage();
+                  },
+                  redirect: (context, state) => _unAuthened(),
+                  routes: [
+                    GoRoute(
+                        path: RequirementDetailRoute.tag,
+                        name: RequirementDetailRoute.name,
+                        builder: (context, state) {
+                          return RequirementDetailPage(id: state.pathParameters['requirement_id']);
+                        })
+                  ]),
+              GoRoute(
+                path: ManagementRoute.tag,
+                name: ManagementRoute.name,
+                builder: (context, state) {
+                  return const ManagementPage();
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
+            ],
+            onException: (context, state, router) {
+              router.go(LoginRoute.tag);
             },
-            redirect: (context, state) => _unAuthened(),
+          )
+        : GoRouter(
+            navigatorKey: _rootNavigatorKey,
+            initialLocation: LoginRoute.tag,
             routes: [
               GoRoute(
-                  path: ArtistRegisterDetailRoute.tag,
-                  name: ArtistRegisterDetailRoute.name,
+                  path: LoginRoute.tag,
+                  name: LoginRoute.name,
                   builder: (context, state) {
-                    return ArtistRegisterDetailPage(id: state.pathParameters['artistRegister_id']);
+                    return const LoginPage();
                   }),
-            ]),
-        GoRoute(
-            path: ArtworkRoute.tag,
-            name: ArtworkRoute.name,
-            builder: (context, state) {
-              return const ArtworkPage();
-            },
-            redirect: (context, state) => _unAuthened(),
-            routes: [
               GoRoute(
-                  path: ArtworkDetailRoute.tag,
-                  name: ArtworkDetailRoute.name,
-                  builder: (context, state) {
-                    return ArtworkDetailPage(id: state.pathParameters['artwork_id']);
-                  }),
-            ]),
-        GoRoute(
-            path: OrderRoute.tag,
-            name: OrderRoute.name,
-            builder: (context, state) {
-              return const OrderPage();
-            },
-            redirect: (context, state) => _unAuthened(),
-            routes: [
+                path: DashboardRoute.tag,
+                name: DashboardRoute.name,
+                builder: (context, state) {
+                  return const DashboardPage();
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
               GoRoute(
-                  path: OrderDetailRoute.tag,
-                  name: OrderDetailRoute.name,
-                  builder: (context, state) {
-                    return OrderDetailPage(id: state.pathParameters['order_id']);
-                  })
-            ]),
-        GoRoute(
-            path: RequirementRoute.tag,
-            name: RequirementRoute.name,
-            builder: (context, state) {
-              return const RequirementPage();
-            },
-            redirect: (context, state) => _unAuthened(),
-            routes: [
+                path: AccountDetailRoute.tag,
+                name: AccountDetailRoute.name,
+                builder: (context, state) {
+                  return AccountDetailPage(id: state.pathParameters['account_id']);
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
               GoRoute(
-                  path: RequirementDetailRoute.tag,
-                  name: RequirementDetailRoute.name,
+                path: ProfileUserRouter.tag,
+                name: ProfileUserRouter.name,
+                builder: (context, state) {
+                  return ProfileUserPage(id: state.pathParameters['user_id']);
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
+              GoRoute(
+                path: ArtistRoute.tag,
+                name: ArtistRoute.name,
+                builder: (context, state) {
+                  return const ArtistPage();
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
+              GoRoute(
+                path: CustomerRoute.tag,
+                name: CustomerRoute.name,
+                builder: (context, state) {
+                  return const CustomerPage();
+                },
+                redirect: (context, state) => _unAuthened(),
+              ),
+              GoRoute(
+                  path: ArtworkRoute.tag,
+                  name: ArtworkRoute.name,
                   builder: (context, state) {
-                    return RequirementDetailPage(id: state.pathParameters['requirement_id']);
-                  })
-            ]),
-        if (PrefUtils().getRole() == 'Admin')
-          GoRoute(
-            path: ManagementRoute.tag,
-            name: ManagementRoute.name,
-            builder: (context, state) {
-              return const ManagementPage();
+                    return const ArtworkPage();
+                  },
+                  redirect: (context, state) => _unAuthened(),
+                  routes: [
+                    GoRoute(
+                        path: ArtworkDetailRoute.tag,
+                        name: ArtworkDetailRoute.name,
+                        builder: (context, state) {
+                          return ArtworkDetailPage(id: state.pathParameters['artwork_id']);
+                        }),
+                  ]),
+              GoRoute(
+                  path: OrderRoute.tag,
+                  name: OrderRoute.name,
+                  builder: (context, state) {
+                    return const OrderPage();
+                  },
+                  redirect: (context, state) => _unAuthened(),
+                  routes: [
+                    GoRoute(
+                        path: OrderDetailRoute.tag,
+                        name: OrderDetailRoute.name,
+                        builder: (context, state) {
+                          return OrderDetailPage(id: state.pathParameters['order_id']);
+                        })
+                  ]),
+              GoRoute(
+                  path: RequirementRoute.tag,
+                  name: RequirementRoute.name,
+                  builder: (context, state) {
+                    return const RequirementPage();
+                  },
+                  redirect: (context, state) => _unAuthened(),
+                  routes: [
+                    GoRoute(
+                        path: RequirementDetailRoute.tag,
+                        name: RequirementDetailRoute.name,
+                        builder: (context, state) {
+                          return RequirementDetailPage(id: state.pathParameters['requirement_id']);
+                        })
+                  ]),
+            ],
+            onException: (context, state, router) {
+              router.go(LoginRoute.tag);
             },
-            redirect: (context, state) => _unAuthened(),
-          ),
-      ],
-      onException: (context, state, router) {
-        router.go(LoginRoute.tag);
-      },
-    );
+          );
   }
 
   static String? _unAuthened() {
